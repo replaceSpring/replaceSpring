a8fc8ec65a25edd8435a56e098a3183de3ae34dc
alert-Enoch <alert4739@gmail.com>
2023-08-01 23:32:40 +0900

    init설정

---
+++ src/main/java/com/app/replace/ReplaceApplication.java
@@ -0,0 +1,13 @@
+package com.app.replace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/replace/mapper/MapperTests.java
@@ -0,0 +1,11 @@
+package com.app.replace.mapper;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class MapperTests {
+
+}
