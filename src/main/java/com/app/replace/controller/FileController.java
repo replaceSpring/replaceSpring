9d55947ec1740209a1b2d72d1fdd1effe159a07b
dev-PatrickLee <dlgudwn99@gmail.com>
2023-08-20 14:18:15 +0900

    mypage update

---
+++ src/main/java/com/app/replace/controller/FileController.java
@@ -0,0 +1,11 @@
+package com.app.replace.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/file/*")
+public class FileController {
+}
