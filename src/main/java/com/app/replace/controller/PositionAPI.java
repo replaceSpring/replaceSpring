e9173e2a0b94dd12de0a1a6c6376f7895472dbd6
dev-aprilchoi <aprilchoi.dev@gmail.com>
2023-09-02 11:48:37 +0900

    final

--- src/main/java/com/app/replace/controller/PositionAPI.java
+++ src/main/java/com/app/replace/controller/PositionAPI.java
@@ -4,6 +4,7 @@ import com.app.replace.dto.PositionDTO;
 import com.app.replace.dto.Search;
 import com.app.replace.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/positions/api/*")
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class PositionAPI {
     private final SearchService searchService;
     @GetMapping("list")
     public List<PositionDTO> getList(Search search){
+        log.info("search keyword : {}", search.toString());
         return searchService.getList(search);
     }
 }
